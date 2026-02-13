64e0f9df47e4a2e947b43ece0ab37eed3f17e429
leraaj <leraalfredojohn@gmail.com>
2026-02-13 19:52:46 +0800

    new features 1

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -25,7 +25,7 @@ export default function Dashboard() {
     const fetchMessages = async () => {
       try {
         const res = await fetch(
-          `${import.meta.env.VITE_API_URL}/api/messages-new`,
+          `${import.meta.env.VITE_SERVER_URL}/api/messages-new`,
           {
             credentials: "include",
             method: "GET",
